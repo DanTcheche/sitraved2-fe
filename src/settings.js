c7e599d9ec729daa7222b234bf79eb6af553bdb9
Dan Tcheche <dtchechenistky@devartis.com>
2021-04-30 12:42:24 -0300

    Fix url

--- src/settings.js
+++ src/settings.js
@@ -1,3 +1,3 @@
 export default {
-  SITRADEV_API_URL: process.env.VUE_APP_SITRADEV_API_URL || "https://sitraved2.herokuapp.com/"
+  SITRADEV_API_URL: process.env.VUE_APP_SITRADEV_API_URL || "https://sitraved2.herokuapp.com/api/"
 };
